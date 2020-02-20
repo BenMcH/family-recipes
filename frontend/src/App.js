acaa5dd1577c7deaff5e1566b5142d80f9cf1384
Ben McHone <benjamin.mchone@gmail.com>
2020-02-19 20:39:09 -0600

    Created basis for recipe cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,16 +11,32 @@ const useRecipes = () => {
   return recipes;
 }
 
+const Recipe = ({recipe}) => (
+<div>
+  <h2>{recipe.name}</h2>
+  {recipe.mealMedia.length && <img src={`http://localhost:1337/${recipe.mealMedia[0].url}`} alt={'Recipe cover image'} />}
+  <p>{recipe.description}</p>
+  <h3>Ingredients</h3>
+  <ul>
+    {recipe.ingredients.map((ingredent) => <li>{ingredent.name} - {ingredent.quantity}</li>)}
+  </ul>
+  <h3>Instructions</h3>
+  <ol>
+    {recipe.instructions.split('\n').map((instruction) => <li>{instruction}</li>)}
+  </ol>
+</div>
+)
+
 const App = () => {
   const recipes = useRecipes();
 
   return (
     <div>
-      <h2>Recipes ({recipes.length})</h2>
+      <h1>Recipes ({recipes.length})</h1>
 
-      <ul>
-        {recipes.map((recipe) => <li>{recipe.name} - {recipe.description}</li>)}
-      </ul>
+      <div id="container">
+        {recipes.map((recipe) => <Recipe recipe={recipe} />)}
+      </div>
 
     </div>
   )
