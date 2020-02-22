483348f496380979aeea259696670781e711dc60
Ben McHone <benjamin.mchone@gmail.com>
2020-02-21 21:03:05 -0600

    Added basic card display for recipes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import {Card, CardContent, Collapse, CardActions, IconButton} from '@material-ui/core';
 import './App.css';
 
 const useRecipes = () => {
@@ -13,21 +14,40 @@ const useRecipes = () => {
   return recipes;
 }
 
-const Recipe = ({recipe}) => (
-<div>
-  <h2>{recipe.name}</h2>
-  {recipe.mealMedia.length && <img src={`http://localhost:1337/${recipe.mealMedia[0].url}`} alt={'Recipe cover image'} />}
-  <p>{recipe.description}</p>
-  <h3>Ingredients</h3>
-  <ul>
-    {recipe.ingredients.map((ingredent) => <li>{ingredent.name} - {ingredent.quantity}</li>)}
-  </ul>
-  <h3>Instructions</h3>
-  <ol>
-    {recipe.instructions.map((instruction) => <li>{instruction.text}</li>)}
-  </ol>
-</div>
-)
+const Recipe = ({recipe}) => {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => setOpen(!open);
+
+  return (
+    <Card variant="outlined">
+      <CardContent>
+        <h2>{recipe.name}</h2>
+        {recipe.mealMedia.length && <img src={`http://localhost:1337/${recipe.mealMedia[0].url}`} alt={'Recipe cover image'} />}
+        <p>{recipe.description}</p>
+        <CardActions disableSpacing style={{display: 'flex', flexDirection: 'row', justifyContent: 'end'}}>
+          <IconButton
+            onClick={toggle}
+            aria-expanded={open}
+            aria-label="show details"
+          >
+            {open ? '-' : '+'}
+          </IconButton>
+        </CardActions>
+        <Collapse in={open}>
+          <h3>Ingredients</h3>
+          <ul>
+            {recipe.ingredients.map((ingredent) => <li>{ingredent.name} - {ingredent.quantity}</li>)}
+          </ul>
+          <h3>Instructions</h3>
+          <ol>
+            {recipe.instructions.map((instruction) => <li>{instruction.text}</li>)}
+          </ol>
+        </Collapse>
+      </CardContent>
+    </Card>
+  );
+}
 
 const App = () => {
   const recipes = useRecipes();
